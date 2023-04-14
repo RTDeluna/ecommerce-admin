60bd5ddd26ffc5fca32ee112d400ba7bcabd0a84
RTDeluna <ramueldeluna30@gmail.com>
2023-04-14 17:38:36 +0800

    minor change

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,10 +6,11 @@ import GoogleProvider from 'next-auth/providers/google';
 const adminEmails = process.env.ADMIN_EMAIL;
 
 export const authOptions = {
+	secret: process.env.SECRET,
 	providers: [
 		GoogleProvider({
-			clientId: process.env.GOOGLE_ID,
-			clientSecret: process.env.GOOGLE_SECRET,
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 		}),
 	],
 	adapter: MongoDBAdapter(clientPromise),
--- pages/api/categories.js
+++ pages/api/categories.js
@@ -1,6 +1,5 @@
 import { mongooseConnect } from '@/lib/mongoose';
 import { Category } from '@/models/Category';
-
 import { isAdminRequest } from '@/pages/api/auth/[...nextauth]';
 
 export default async function handle(req, res) {
